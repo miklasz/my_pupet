528eba412fbaf8ecdbec6a8e7726a5f465746eb8
Bartosz Miklaszewski <bart@datacentred.co.uk>
2017-05-20 22:21:34 +0100

    aqq

--- site.pp
+++ site.pp
@@ -1,3 +1,4 @@
+1
 file {'/tmp/it_works.txt':                        # resource type file and filename
   ensure  => present,                             # make sure it exists
   mode    => '0644',                              # file permissions
